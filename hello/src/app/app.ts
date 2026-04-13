59dbda1a18556b8e879bdfb9f9b8eb2a1fda10ea
Pedro Ricardo dos Santos Gomes <rpedroricardo87@gmail.com>
2026-04-13 17:25:03 +0000

    feat: alterando template

--- hello/src/app/app.ts
+++ hello/src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonMod } ;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterOutlet, FormsModule, CommonModule],
+  template: ` <main>
+    <h1 class="text-center"> {{title()}} </h1>
+    <input [(ngModel)] = "name"/>
+    <p> Hello {{name}} </p>
+    <button [disabled] = "isButtonDisabled" (click) = "handleclick()"> Click Here </button>
+
+    <router-outlet></router-outlet>
+  </main>`,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('hello');
+  isButtonDisabled = true;
+  protected readonly title = signal('O Guardiorla vem aí');
+  name = "Pedro";
+  handleclick() {
+    window.alert("Vasco é o maior que já existiu")
+  }
 }
