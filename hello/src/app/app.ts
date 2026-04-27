dc35d14f558d49a4850ee5162669c49b80ce78e2
Pedro Ricardo dos Santos Gomes <rpedroricardo87@gmail.com>
2026-04-27 17:27:27 +0000

    ajustando app

--- hello/src/app/app.ts
+++ hello/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -14,7 +14,7 @@ import { CardModule } from 'primeng/card';
   selector: 'app-root',
   imports: [RouterOutlet, FormsModule, CommonModule, FloatLabelModule, InputTextModule, ButtonModule, CardModule],
   template: ` <main class="text-center m-auto">
-    <h1 class="text-center"> {{title}} </h1>
+    <h1 class="text-center"> {{ titulo }} </h1>
     
     <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
             
@@ -26,43 +26,59 @@ import { CardModule } from 'primeng/card';
     <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
             
             <p-floatlabel variant="on">
-                <input pInputText id="on_labell" [(ngModel)]="texto" autocomplete="off" />
+                <input pInputText id="on_labell" [(ngModel)]="textoescrito" autocomplete="off" />
                 <label for="on_labell">Texto da Lenda</label>
             </p-floatlabel>
-        </div>
+    </div>
+    <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
+            
+            <p-floatlabel variant="on">
+                <input pInputText id="on_labell" [(ngModel)]="foto" autocomplete="off" />
+                <label for="on_labell">Foto da Lenda</label>
+            </p-floatlabel>
+    </div>
     <div class="card flex justify-center mt-4">
             <p-button label="Clica aí" (onClick)="handleclick()" />
     </div>
     
 
     <div class="mb-4 p-8 flex items-center justify-center">
-            <p-card [style]="{ width: '25rem', overflow: 'hidden' }">
-                <ng-template #header>
-                    <img alt="Card" class="w-full" src="https://primefaces.org/cdn/primeng/images/card-ng.jpg" />
-                </ng-template>
-                <ng-template #title> {{name}} </ng-template>
-                <ng-template #subtitle> Notícias do dia </ng-template>
-                <p>
-                    {{texto}}
-                </p>
-                <ng-template #footer>
-                    <div class="flex gap-4 mt-1">
-                        <p-button label="Cancel" severity="secondary" class="w-full" [outlined]="true" styleClass="w-full" />
-                        <p-button label="Save" class="w-full" styleClass="w-full" />
-                    </div>
-                </ng-template>
-            </p-card>
-        </div>
+        <p-card [style]="{ width: '25rem', overflow: 'hidden' }">
+          <ng-template pTemplate="header">
+            <img alt="Card" class="w-full" [src]="cardPhoto" />
+          </ng-template>
+          <ng-template pTemplate="title">
+            {{ title }}
+          </ng-template>
+          <ng-template pTemplate="subtitle"> Notícias do dia </ng-template>
+          <p>
+            {{ cardText }}
+          </p>
+          <ng-template pTemplate="footer">
+            <div class="flex gap-4 mt-1">
+              <p-button label="Cancel" severity="secondary" class="w-full" [outlined]="true" styleClass="w-full" />
+              <p-button label="Save" class="w-full" styleClass="w-full" />
+            </div>
+          </ng-template>
+        </p-card>
+      </div>
     <router-outlet></router-outlet>
   </main>`,
   styleUrl: './app.css'
 })
 export class App {
   isButtonDisabled = false;
-  protected readonly title = signal('O Guardiorla vem aí');
-  name = "Pedro";
-  texto = "texto da lenda";
+  titulo = 'Lendas Urbanas';
+  title = '';
+  name = '';
+  textoescrito = '';
+  foto = '';
+  cardText = '';
+  cardPhoto = 'https://primefaces.org/cdn/primeng/images/card-ng.jpg';
   handleclick() {
-    window.alert("Vasco é o maior que já existiu")
+    this.titulo = this.name;
+    this.title = this.name;
+    this.cardText = this.textoescrito;
+    this.cardPhoto = this.foto || this.cardPhoto;
   }
 }
